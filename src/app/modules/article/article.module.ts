ff22f3e1402f91f31b84a91182c64a833de3d6cc
trungsyk58utc <cayconlontonzz@gmail.com>
2021-12-14 16:58:26 +0700

    update-feature

--- src/app/modules/article/article.module.ts
+++ src/app/modules/article/article.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleComponent } from './article.component';
 import { MatInputModule } from '@angular/material/input';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { FormsModule } from '@angular/forms';
 import { LoadingSpinnerModule } from 'src/app/shared/components/loading-spinner/loading-spinner.module';
 import { DebounceClickDirectives } from './directives/debounces.directive';
-
+import { LoadingFullscreenModule } from 'src/app/shared/components/loading-fullscreen/loading-fullscreen.module';
 
 @NgModule({
-  declarations: [ArticleComponent,DebounceClickDirectives],
+  declarations: [ArticleComponent, DebounceClickDirectives],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: ':slug', component: ArticleComponent }]),
@@ -18,6 +18,7 @@ import { DebounceClickDirectives } from './directives/debounces.directive';
     FormsModule,
     LoadingSpinnerModule,
     MatPaginatorModule,
+    LoadingFullscreenModule,
   ],
 })
 export class ArticleModule {}
